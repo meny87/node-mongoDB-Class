1edefd5709fcc054f5b10fe4b9613e4b74e36c46
Emmanuel Casas <emmanuelcasas@MacBook-Pro-de-Emmanuel.local>
2018-08-16 14:29:36 -0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -26,6 +26,15 @@ app.post('/todos', (req,res) =>{
   });
 });
 
+// GET routes
+app.get('/todos', (req, res) =>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  }, (e) =>{
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, () =>{
   console.log('Started on port 3000');
 });
