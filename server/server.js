ed125b3636396f73ccbd05dad4d952faab3abdc3
Emmanuel Casas <emmanuelcasas@MacBook-Pro-de-Emmanuel.local>
2018-08-21 11:35:12 -0500

    Test Cases for DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -72,17 +72,12 @@ app.get('/todos/:id', (req, res) => {
 
 // DElete routes
 app.delete('/todos/:id', (req, res) => {
-  //Get the id;
+
   var id = req.params.id;
-  //validate ID -> not valid? return 404
+
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  //remove todo by id
-  //sucess
-  //if not doc, send 404
-  //error
-  //400 with empty value
 
   Todo.findByIdAndRemove(id).then((todo) => {
     if (!todo) {
@@ -93,7 +88,7 @@ app.delete('/todos/:id', (req, res) => {
     });
   }).catch((e) => {
     res.status(400).send();
-  })
+  });
 });
 
 app.listen(port, () => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -118,5 +118,45 @@ describe('GET /todos/:id ', () => {
       .end(done);
   });
 
+});
 
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = dummyTodos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        };
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done());
+
+        //query database using findById toNo
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object ID is invalid', (done) => {
+    request(app)
+      .delete(`/todos/12312`)
+      .expect(404)
+      .end(done);
+  });
 });
