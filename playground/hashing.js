c619965b86b1e8d2af7c95fc153d34176de1a78d
Emmanuel Casas <emmanuelcasas@MacBook-Pro-de-Emmanuel.local>
2018-08-30 16:55:36 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+//bcrypt.genSalt(10, (err,salt) =>{
+//   bcrypt.hash(password, salt, (err, hash) =>{
+//     console.log(hash);
+//   })
+// });
+
+var hashedPassword = '$2a$10$EiQjmJdUmKsBYYD5Vk6e.e5u53Ru18Otwh6HlfARWHeo/PhROZ7j';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 //
@@ -24,13 +39,13 @@ const jwt = require('jsonwebtoken');
 //   console.log('Data was changed');
 // }
 
-var data ={
-  id: 1023123,
-  text: 'hello'
-};
-
-var token = jwt.sign(data, 'somesecret');
-console.log(token);
-
-var decoded = jwt.verify(token, 'somesecret');
-console.log(decoded);
+// var data ={
+//   id: 1023123,
+//   text: 'hello'
+// };
+//
+// var token = jwt.sign(data, 'somesecret');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, 'somesecret');
+// console.log(decoded);
