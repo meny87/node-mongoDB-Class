f9748b80ed1714bd05a81fee667f1b787293ecc9
Emmanuel Casas <emmanuelcasas@MacBook-Pro-de-Emmanuel.local>
2018-09-01 16:13:49 -0500

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -33,26 +33,32 @@ var UserSchema = new mongoose.Schema({
   }]
 });
 
-UserSchema.methods.toJSON = function () {
+UserSchema.methods.toJSON = function() {
   var user = this;
   var userObject = user.toObject();
 
   return _.pick(userObject, ['_id', 'email']);
 };
 
-UserSchema.methods.generateAuthToken = function () {
+UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  var token = jwt.sign({
+    _id: user._id.toHexString(),
+    access
+  }, 'abc123').toString();
 
-  user.tokens.push({access, token});
+  user.tokens.push({
+    access,
+    token
+  });
 
   return user.save().then(() => {
     return token;
   });
 };
 
-UserSchema.statics.findByToken = function (token) {
+UserSchema.statics.findByToken = function(token) {
   var User = this;
   var decoded;
 
@@ -69,21 +75,45 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
-UserSchema.pre('save', function (next) {
+UserSchema.pre('save', function(next) {
   var user = this;
 
   if (user.isModified('password')) {
-    bcrypt.genSalt(10, (err,salt) =>{
-      bcrypt.hash(user.password, salt, (err, hash) =>{
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
         next();
       });
     });
-  }else{
+  } else {
     next();
   }
 });
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({
+    email
+  }).then((user) => {
+    if (!user) {
+      return Promise.reject({error: 'Invalid User'});
+    };
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject({error: 'Invalid Password'});
+        }
+      });
+    });
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User}
+module.exports = {
+  User
+}
